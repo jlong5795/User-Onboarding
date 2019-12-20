02fdcf60c05091e93f28c6d3afdbf2a5069aaab9
Jason Long <jlong5795@gmail.com>
2019-12-19 22:44:34 -0600

    Completed MVP

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {withFormik, Form, Field} from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 
 const AppForm = ({values, errors, touched, status}) => {
+    const [user, setUser] = useState([]);
+    useEffect(() => {
+        console.log('Status has changed', status);
+        status && setUser( user => [...user, status]);
+    },[status]);
     return(
         <div className='app-form'>
             <Form>
@@ -49,32 +54,51 @@ const AppForm = ({values, errors, touched, status}) => {
                         type='checkbox'
                         name='tosCheckbox'
                         id='tosCheckbox'
+                        check={values.tosCheckbox}
                     />
                     {touched.checkbox && errors.checkbox && <p className='errors'>{errors.checkbox}</p>}
                 </label>
                 </p>
                 <button type='submit'>Submit</button>
             </Form>
+            {user.map(item => (
+                <ul key={item.id}>
+                    <li>Name: {item.name}</li>
+                    <li>Email: {item.email}</li>
+                </ul>
+            ))}
         </div>
     )
 }
 
 const FormikAppForm = withFormik({
-    mapPropsToValues({name, email, checkbox}){
+    mapPropsToValues({name, email, tosCheckbox}){
         return{
         name: name || '',
         email: email || '',
         password: '',
-        checkbox: checkbox || false
+        tosCheckbox: tosCheckbox || false
         }
     },
     validationSchema: Yup.object().shape({
-        name: Yup.string().required().min(3),
-        email: Yup.string().required().email(),
-        password: Yup.string().required().min(8),
-        checkbox: Yup.boolean(true, 'Must accept term of service to continue')
+        name: Yup.string().required().min(3, 'Name must be longer than 3 characters'),
+        email: Yup.string().required().email('Please enter a VALID email address'),
+        password: Yup.string().required().min(8, 'Passwords must be at least 8 characters'),
+        tosCheckbox: Yup.boolean().oneOf([true], 'Must accept term of service to continue')
 
-    })
+    }),
+    handleSubmit(values, {setStatus, resetForm}) {
+        console.log('Submitting', values);
+        axios.post('https://reqres.in/api/users', values)
+        .then(response => {
+            console.log('Success!', response);
+            setStatus(response.data);
+            resetForm();
+        })
+        .catch(error => {
+            console.log('There was an error', error.response);
+        })
+    }
 })(
     AppForm
 );
